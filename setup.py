f63d580a8696621562b7ca58190f576ee0736d7e
Hrithik Malhotra <44829171+hrithik18236@users.noreply.github.com>
2020-06-19 18:35:39 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='prob-gauss-normal',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['prob-gauss-normal'],
+      author = 'Hrithik Malhotra',
+      author_email = 'hrithik18236@iiitd.ac.in',
+      zip_safe=False)
